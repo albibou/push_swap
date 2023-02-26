8daa2e2362798c02a8dbcc7bad16d89f8b988708
Alban TARDIF <atardif@e2r11p5.clusters.42paris.fr>
2023-02-26 18:21:29 +0100

    Push swap 26-02-23

--- src/trichelou.c
+++ src/trichelou.c
@@ -6,12 +6,30 @@
 /*   By: atardif <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:12:49 by atardif           #+#    #+#             */
-/*   Updated: 2023/02/24 15:44:05 by atardif          ###   ########.fr       */
+/*   Updated: 2023/02/26 18:19:13 by atardif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	is_in_chunk(int index, int chunk)
+{
+	if (index >= 0 && index <= chunk)
+		return (1);
+	return (0);
+}
+
+void	best_move(t_list **stack, int chunk)
+{
+	int	firstpos;
+	int	lastpos;
+	t_list	*tmp;
+
+	tmp = *stack;
+	while(tmp->next != NULL)
+	{
+	}
+}
 
 int	in_chunk(t_list **stack_a, int chunk)
 {
@@ -36,7 +54,10 @@ void	pre_sort(t_list **stack_a, t_list **stack_b, int ratio, int chunk)
 	else if ((*stack_a)->index > (chunk - ratio / 2) && (*stack_a)->index <= chunk)
 	{
 		pb(stack_b, stack_a);
-		rb(stack_b);
+		if (is_in_chunk((*stack_a)->index, chunk))
+			rb(stack_b);
+		else
+			rr(stack_a, stack_b);
 	}
 	else
 		ra(stack_a);
@@ -78,56 +99,146 @@ void	insert_back_sort(t_list **stack_a, t_list **stack_b, int size)
 	}
 }
 
-
-
-
-/*void	third_phase(t_list **stack_a, t_list **stack_b)
+void	first_phase10(t_list **stack_a, t_list **stack_b, int size)
 {
-	t_list	*tmp;
-	int	i;
+	int	chunk;
+	int	chunk2;
+	int	chunk3;
+	int	chunk4;
+	int	chunk5;
+	int	chunk6;
+	int	chunk7;
+	int	chunk8;
+	int	chunk9;
 
-	while(ft_lstsize(*stack_b) > 0)
-	{
-		i = 0;
-		tmp = ft_lstlast(*stack_b);
-		if( tmp->index > (*stack_b)->index)
-			rrb(stack_b);
-		while ((*stack_b)->index > (*stack_a)->index)
-		{
-			ra(stack_a);
-			i++;
-		}
-		if ((*stack_b)->index < (*stack_a)->index)
-			pa(stack_a, stack_b);
-		while (i > 0)
-		{
-			rra(stack_a);
-			i--;
-		}
-	}
+	chunk = size / 10 - 1;
+	chunk2 = (chunk * 2) + 1;
+	chunk3 = (chunk * 3) + 1;
+	chunk4 = (chunk * 4) + 1;
+	chunk5 = (chunk * 5) + 1;
+	chunk6 = (chunk * 6) + 1;
+	chunk7 = (chunk * 7) + 1;
+	chunk8 = (chunk * 8) + 1;
+	chunk9 = (chunk * 9) + 1;
+	while (!in_chunk(stack_a, chunk) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk);
+	while (!in_chunk(stack_a, chunk2) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk2);
+	while (!in_chunk(stack_a, chunk3) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk3);
+	while (!in_chunk(stack_a, chunk4) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b,chunk, chunk4);
+	while (!in_chunk(stack_a, chunk5) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk5);
+	while (!in_chunk(stack_a, chunk6) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk6);
+	while (!in_chunk(stack_a, chunk7) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk7);
+	while (!in_chunk(stack_a, chunk8) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk8);
+	while (!in_chunk(stack_a, chunk9) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk9);
+	while (!in_chunk(stack_a, size - 4) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, size - 4);
+}
+
+/*void	first_phase9(t_list **stack_a, t_list **stack_b, int size)
+{
+	int	chunk;
+	int	chunk2;
+	int	chunk3;
+	int	chunk4;
+	int	chunk5;
+	int	chunk6;
+	int	chunk7;
+	int	chunk8;
+
+	chunk = size / 9 - 1;
+	chunk2 = (chunk * 2) + 1;
+	chunk3 = (chunk * 3) + 1;
+	chunk4 = (chunk * 4) + 1;
+	chunk5 = (chunk * 5) + 1;
+	chunk6 = (chunk * 6) + 1;
+	chunk7 = (chunk * 7) + 1;
+	chunk8 = (chunk * 8) + 1;
+	while (!in_chunk(stack_a, chunk) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk);
+	while (!in_chunk(stack_a, chunk2) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk2);
+	while (!in_chunk(stack_a, chunk3) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk3);
+	while (!in_chunk(stack_a, chunk4) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b,chunk, chunk4);
+	while (!in_chunk(stack_a, chunk5) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk5);
+	while (!in_chunk(stack_a, chunk6) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk6);
+	while (!in_chunk(stack_a, chunk7) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk7);
+	while (!in_chunk(stack_a, chunk8) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk8);
+	while (!in_chunk(stack_a, size - 4) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, size - 4);
 }*/
 
+void	first_phase4(t_list **stack_a, t_list **stack_b, int size)
+{
+	int	chunk;
+	int	chunk2;
+	int	chunk3;
 
-void	first_phase(t_list **stack_a, t_list **stack_b, int ratio, int size)
+	chunk = size / 4 - 1;
+	chunk2 = (chunk * 2) + 1;
+	chunk3 = (chunk * 3) + 1;
+	while (!in_chunk(stack_a, chunk) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk);
+	while (!in_chunk(stack_a, chunk2) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk2);
+	while (!in_chunk(stack_a, chunk3) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk3);
+	while (!in_chunk(stack_a, size - 4) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, size - 4);
+}
+
+
+/*void	first_phase3(t_list **stack_a, t_list **stack_b, int size)
+{
+	int	chunk;
+	int	chunk2;
+
+	chunk = size / 3 - 1;
+	chunk2 = (chunk * 2) + 1;
+	while (!in_chunk(stack_a, chunk) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk);
+	while (!in_chunk(stack_a, chunk2) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, chunk2);
+	while (!in_chunk(stack_a, size - 4) && ft_lstsize(*stack_a) > 3)
+		pre_sort(stack_a, stack_b, chunk, size - 4);
+}*/
+
+/*void	first_phase2(t_list **stack_a, t_list **stack_b, int size)
 {
 	int	chunk;
 
-	chunk = (ratio * 2) + 1;
-	while (!in_chunk(stack_a, ratio) && ft_lstsize(*stack_a) > 3)
-		pre_sort(stack_a, stack_b, 0, ratio);
+	chunk = size / 2 - 1;
 	while (!in_chunk(stack_a, chunk) && ft_lstsize(*stack_a) > 3)
-		pre_sort(stack_a, stack_b, ratio, chunk);
+		pre_sort(stack_a, stack_b, chunk, chunk);
 	while (!in_chunk(stack_a, size - 4) && ft_lstsize(*stack_a) > 3)
-		pre_sort(stack_a, stack_b, ratio, size - 4);
-}
+		pre_sort(stack_a, stack_b, chunk, size - 4);
+}*/
 
 void	tri_chelou(t_list **stack_a, t_list **stack_b, t_data *data)
 {
-	int	ratio;
-
-	ratio = data->a_size / 3 - 1;
-	first_phase(stack_a, stack_b, ratio, data->a_size);
+	if (data->a_size < 30)
+	{
+		pre_sort(stack_a, stack_b, data->a_size, data->a_size);
+		insert_back_sort(stack_a, stack_b, data->a_size);
+		return;
+	}
+	else if (data->a_size < 200)
+		first_phase4(stack_a, stack_b, data->a_size);
+	else if (data->a_size >= 200)
+		first_phase10(stack_a, stack_b, data->a_size);
 	sort_3(stack_a);
 	insert_back_sort(stack_a, stack_b, data->a_size - 4);
-	//third_phase(stack_a, stack_b);
 }
